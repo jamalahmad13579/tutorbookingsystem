9498473174f3fb48d0888db684510f3a89765219
jamalahmad13579 <jamaluddin_ahmad@hotmail.co.uk>
2015-10-18 21:37:42 +0100

    this file is called email.aspx.cs

---
+++ email.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//using System.Windows.Forms;
+using System.Net.Mail;
+
+public partial class email : System.Web.UI.Page
+{
+
+   
+}
